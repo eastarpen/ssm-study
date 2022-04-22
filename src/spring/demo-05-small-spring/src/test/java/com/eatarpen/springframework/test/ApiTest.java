8ae8b39509d09d04a02b3b00279a997fb2e364ec
Eastarpen <eaststarpen@gmail.com>
2022-04-22 21:52:02 +0800

    demo-05 chapter-03 note

--- src/spring/demo-05-small-spring/src/test/java/com/eatarpen/springframework/test/ApiTest.java
+++ src/spring/demo-05-small-spring/src/test/java/com/eatarpen/springframework/test/ApiTest.java
@@ -1,10 +1,23 @@
 package com.eatarpen.springframework.test;
 
+import com.eastarpen.springframework.beans.factory.config.BeanDefinition;
+import com.eastarpen.springframework.beans.factory.support.DefaultListableBeanFactory;
+import com.eatarpen.springframework.test.bean.UserService;
 import org.junit.Test;
 
 public class ApiTest {
 
     @Test
     public void testBeanFactory() {
-   }
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
+
+        String name = "userService";
+        beanFactory.registerBeanDefinition(name, beanDefinition);
+
+        UserService userService = (UserService) beanFactory.getBean(name);
+
+        beanFactory.getBean("noSuchBean");
+    }
 }
