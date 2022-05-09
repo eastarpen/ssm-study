9adb78cb18fbc24adb55de2db9aabc826686176c
Eastarpen <eaststarpen@gmail.com>
2022-05-09 08:18:22 +0800

    demo-06 delete

--- src/springmvc/demo-06-thymeleaf-start/src/main/java/com/eastarpen/demo06/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.eastarpen.demo06.service;
-
-import com.eastarpen.demo06.utils.LoginResult;
-import com.eastarpen.demo06.entity.User;
-
-public interface UserService {
-
-    public LoginResult login(User user);
-}
